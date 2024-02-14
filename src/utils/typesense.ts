8fafffe5d22b38b876fb0e634fbcfee808c7b5f9
Phi Hung Hoang <hphihung795@gmail.com>
2024-02-14 17:55:03 +0700

    fix: use VITE_ env prefix in typesense instantsearch adapter

--- src/utils/typesense.ts
+++ src/utils/typesense.ts
@@ -2,12 +2,12 @@ import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
 
 export const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
-    apiKey: import.meta.env.PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
+    apiKey: import.meta.env.VITE_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
     nodes: [
       {
-        host: import.meta.env.PUBLIC_TYPESENSE_HOST || 'localhost',
-        port: parseInt(import.meta.env.PUBLIC_TYPESENSE_PORT || '8108'),
-        protocol: import.meta.env.PUBLIC_TYPESENSE_PROTOCOL || 'http',
+        host: import.meta.env.VITE_TYPESENSE_HOST || 'localhost',
+        port: parseInt(import.meta.env.VITE_TYPESENSE_PORT || '8108'),
+        protocol: import.meta.env.VITE_TYPESENSE_PROTOCOL || 'http',
       },
     ],
   },
